53d77f5e0ec47da915a3c8d20e7f6653dad06331
mateuszJS <mate.walendzik@gmail.com>
2024-05-11 15:42:58 +0200

    add highlight to code editor

--- src/components/code-block/index.ts
+++ src/components/code-block/index.ts
@@ -6,27 +6,51 @@ import prismStyles from './prism/prism.inline.css'
 // https://prismjs.com/download.html#themes=prism-okaidia&languages=markup+clike+javascript+typescript+wgsl&plugins=highlight-keywords
 
 class CodeBlock extends HTMLElement {
+  private overlayStartNode: HTMLElement
+  private overlayEndNode: HTMLElement
+
+  static observedAttributes = ["highlight-lines"];
+
   constructor() {
     super();
     const shadowRoot = this.attachShadow({mode: 'open'});
     const trimmed = this.innerHTML.trim();
-    
-    let lineNumbersClass = "";
-    if (this.classList.contains("line-numbers")) {
-      lineNumbersClass = "line-numbers";
-    }
 
     shadowRoot.innerHTML = `
       <style>${prismStyles}${ourStyles}</style>
-      <pre class="${lineNumbersClass} ${this.classList}" id="pre"><code id="code">${trimmed}</code><button id="copy-button">Copy</button></pre>
+      <pre id="pre"><code id="code">${trimmed}</code><button id="copy-button">Copy</button></pre>
     `
+    this.overlayStartNode = shadowRoot.querySelector('.overlay-before')!
+    this.overlayEndNode = shadowRoot.querySelector('.overlay-after')!
+    shadowRoot.querySelector('pre')!.classList.add(this.getAttribute('code-lang')!)
 
     Prism.highlightAllUnder(shadowRoot);
+
+    this.overlayStartNode = document.createElement('span')
+    this.overlayStartNode.classList.add('overlay', 'overlay-before')
+    shadowRoot.querySelector('code')!.appendChild(this.overlayStartNode)
+    
+    this.overlayEndNode = document.createElement('span')
+    this.overlayEndNode.classList.add('overlay', 'overlay-after')
+    shadowRoot.querySelector('code')!.appendChild(this.overlayEndNode)
+
 
     const copyButton = shadowRoot.querySelector('#copy-button')!; 
     copyButton.addEventListener("click", () => {
         this.copyCode();                   
     });
+  }
+
+  attributeChangedCallback(name: string, oldValue: string, newValue: string) {
+    console.log(name, oldValue, newValue)
+    switch (name) {
+      case 'highlight-lines': {
+        const [start, end] = newValue.split('-').map(v => parseInt(v, 10))
+        this.overlayStartNode.style.bottom = `calc(100% - (${start - 1}em * 1.5))`
+        this.overlayEndNode.style.top = `calc(${end}em * 1.5)`
+        break;
+      }
+    }
   }
   
   copyCode() {
@@ -41,6 +65,8 @@ class CodeBlock extends HTMLElement {
       console.warn('Oops, unable to copy');  
     } 
   }
+
+
 }
 
 customElements.define('code-block', CodeBlock);
